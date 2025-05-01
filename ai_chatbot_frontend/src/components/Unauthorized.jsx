3d9cdb2a235feb556872a0e721fe041161b7efff
OMPATEL20 <124259661+OMPATEL20@users.noreply.github.com>
2025-04-30 18:51:29 -0600

    Update Unauthorized.jsx

--- ai_chatbot_frontend/src/components/Unauthorized.jsx
+++ ai_chatbot_frontend/src/components/Unauthorized.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Unauthorized = () => (
   <div style={{ textAlign: 'center', marginTop: '50px' }}>
-    <h1 style={{ color: 'red' }}>🚫 Unauthorized Access</h1>
+    <h1 style={{ color: 'red' }}> Unauthorized Access</h1>
     <p>You don't have permission to access this page.</p>
   </div>
 );
